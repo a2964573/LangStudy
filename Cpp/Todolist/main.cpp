06e43578ed907ed598781142ba84c25987f15dd3
a2964573 <a2964573@naver.com>
2024-07-30 23:33:23 +0900

    20240730

--- Cpp/Todolist/main.cpp
+++ Cpp/Todolist/main.cpp
@@ -15,21 +15,23 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    LIST list = {0,};
+
+    int rtn;
     int key;
     while(true) {
         showTitle();
 
-        key = static_cast<int>(getch());
-        if(65 <= key && key <= 90) {
-            key += 32;
-        }
+        memset(&list, 0x00, sizeof(list));
 
+        key = onClickKeyEvent();
         switch(key) {
-            case MODE_INSERT :
+            case MODE_LIST_INSERT :
+                rtn = insertList(_global, list);
             break;
-            case MODE_UPDATE :
+            case MODE_LIST_UPDATE :
             break;
-            case MODE_DELETE :
+            case MODE_LIST_DELETE :
             break;
             case MODE_REFRESH:
                 continue;
@@ -37,6 +39,13 @@ int main(int argc, char *argv[])
                 std::cout << "Bye" << std::endl;
                 return 0;
         }
+
+        if(rtn < 0) {
+            std::cerr << "Error: " << rtn << std::endl;
+            continue;
+        }
+
+        printList(list);
     }
 
     return 0;
@@ -69,15 +78,85 @@ int init(int argc, char *argv[], GLOBAL& _global)
 
 void showTitle() noexcept
 {
-    system("cls");
-    std::cout << "#########################################"   << std::endl;
-    std::cout << "# #####     ###        #          ##### #"   << std::endl;
-    std::cout << "#   #       #  #       #            #   #"   << std::endl;
-    std::cout << "#   #       #   #      #            #   #"   << std::endl;
-    std::cout << "#   #   ##  #   #  ##  #     #  ##  #   #"   << std::endl;
-    std::cout << "#   #  #  # #   # #  # #       #    #   #"   << std::endl;
-    std::cout << "#   #  #  # #   # #  # #     #  ##  #   #"   << std::endl;
-    std::cout << "#   #  #  # #  #  #  # #     #    # #   #"   << std::endl;
-    std::cout << "#   #   ##  ###    ##  ##### #  ##  #   #"   << std::endl;
-    std::cout << "#########################################\n" << std::endl;
+    // system("cls");
+    std::cout << TODOLIST << std::endl;
+}
+
+int onClickKeyEvent() noexcept
+{
+    int key = static_cast<int>(getch());
+    if(65 <= key && key <= 90) {
+        key += 32;
+    }
+
+    return key;
+}
+
+int insertList(GLOBAL& _global, LIST& list)
+{
+    uint tag_cnt    = 0;
+    char title[512] = {0,};
+    char desc[2048] = {0,};
+    std::string input;
+
+    std::cout << "Input now list title." << std::endl;
+    std::cin >> input;
+    if(input.length() > 256) {
+        std::cerr << "list title too long: " << input << "(" << input.length() << "byte)" << std::endl;
+        return -1;
+    }
+    strcpy(title, input.c_str());
+
+    std::cout << "Input now list description." << std::endl;
+    std::cin >> input;
+    if(input.length() > 1024) {
+        std::cerr << "list description too long: " << input << "(" << input.length() << "byte)" << std::endl;
+        return -1;
+    }
+    strcpy(desc, input.c_str());
+
+    std::cout << "Want to add a tag? (y/n)" << std::endl;
+    int key = onClickKeyEvent();
+    switch(key) {
+        case ANSWER_YES :
+            // tag_cnt = editTags(list);
+        break;
+        case ANSWER_NO  :
+            std::cout << "No tag added." << std::endl;
+        break;
+    }
+
+    list.id      = ++_global.last_id;
+    list.index   = ++_global.list_count;
+    list.tag_cnt = tag_cnt;
+
+    strcpy(list.in_date , "00000000");
+    strcpy(list.in_time , "00000000");
+    strcpy(list.title   , title     );
+    strcpy(list.desc    , desc      );
+    strcpy(list.clr_date, "00000000");
+    strcpy(list.clr_time, "00000000");
+
+    return 0;
+}
+
+void printList(const LIST& list) noexcept
+{
+    std::cout << "ID         : " << list.id       << std::endl;
+    std::cout << "Index      : " << list.index    << std::endl;
+    std::cout << "Input Date : " << list.in_date  << std::endl;
+    std::cout << "Input Time : " << list.in_time  << std::endl;
+    std::cout << "Title      : " << list.title    << std::endl;
+    std::cout << "Description: " << list.desc     << std::endl;
+    std::cout << "Clear Date : " << list.clr_date << std::endl;
+    std::cout << "Clear Time : " << list.clr_time << std::endl;
+    std::cout << "Tag Count  : " << list.tag_cnt  << std::endl;
+    std::cout << std::endl;
+}
+
+int editTags(LIST& list)
+{
+    int tag_cnt = 0;
+
+    return tag_cnt;
 }
--- Cpp/Todolist/main.h
+++ Cpp/Todolist/main.h
@@ -8,17 +8,59 @@
 #include <cstdlib>
 #include <conio.h>
 
-#define MODE_INSERT  105
-#define MODE_UPDATE  117
-#define MODE_DELETE  100
-#define MODE_REFRESH 114
-#define MODE_QUIET   113
+#define TODOLIST \
+    "############################################\n" \
+    "# #####      ###         #           ##### #\n" \
+    "#   #        #  #        #             #   #\n" \
+    "#   #        #   #       #             #   #\n" \
+    "#   #   ###  #   #  ###  #     #  ###  #   #\n" \
+    "#   #  #   # #   # #   # #       #     #   #\n" \
+    "#   #  #   # #   # #   # #     #  ###  #   #\n" \
+    "#   #  #   # #  #  #   # #     #     # #   #\n" \
+    "#   #   ###  ###    ###  ##### #  ###  #   #\n" \
+    "############################################\n"
+
+#define ANSWER_YES        121
+#define ANSWER_NO         110
+#define MODE_LIST_INSERT  105
+#define MODE_LIST_UPDATE  117
+#define MODE_LIST_DELETE  100
+#define MODE_TAG_ADD      97
+#define MODE_TAG_END      101
+#define MODE_REFRESH      114
+#define MODE_QUIET        113
+
+using uint = unsigned int;
 
 typedef struct {
-    char filename[1024];
+    char filename [1024];
+    uint list_count;
+    uint last_id;
 } GLOBAL;
+
+typedef struct {
+    uint id;
+    char name     [64  ];
+} TAG;
+
+#define MAX_TAG_COUNT 10
+typedef struct {
+    uint id;
+    int  index;
+    char in_date  [16  ];
+    char in_time  [16  ];
+    char title    [512 ];
+    char desc     [2048];
+    char clr_date [16  ];
+    char clr_time [16  ];
+    uint tag_cnt;
+    TAG  tags     [MAX_TAG_COUNT];
+} LIST;
 
 int init(int argc, char* argv[], GLOBAL& _global);
 void showTitle() noexcept;
+int onClickKeyEvent() noexcept;
+int insertList(GLOBAL& _global, LIST& list);
+void printList(const LIST& list) noexcept;
 
 #endif // MAIN_H
